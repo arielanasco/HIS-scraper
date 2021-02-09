09f4218b55793820130feb9a32c75e15f4036f0c
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-09 10:28:34 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,4 +2,5 @@ from web_driver import WebDriver
 
 site1= WebDriver("https://google.com")
 site1.displaySiteInfo()
-site1.initializeBrowser()
+driver = site1.initializeBrowser()
+print(driver.title)
