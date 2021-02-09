28195ce48873bb5a521d87fd1d84456979074028
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-09 10:38:11 +0800

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -3,13 +3,6 @@ from selenium.webdriver.chrome.options import Options
 import warnings
 
 class WebDriver:
-    options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 1})
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-    warnings.filterwarnings('ignore')
-
     def __init__(self,url):
         self.url = url
     
