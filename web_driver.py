487febf40ace9755b6af15c90a398bde14c60a8d
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 09:27:53 +0800

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -7,6 +7,11 @@ import re
 import pandas as pd
 import random
 class WebDriver:
+    userAgentList = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+        ]
     options = Options()
     options.add_argument('--no-sandbox')
     options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images":2,
@@ -21,17 +26,11 @@ class WebDriver:
                                             })
 
     options.add_argument("--headless")
-    options.add_argument(f'--user-agent={self.user_agent}')
+    options.add_argument(f'--user-agent={sample(userAgentList,1)[0]}')
     warnings.filterwarnings('ignore')
     driver = webdriver.Chrome(options=options)
-    userAgentList = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
-        ]
-    def __init__(self,url,user_agent = sample(userAgentList,1)[0]):
+    def __init__(self,url):
         self.url = url
-        self.user_agent = user_agent
 
     def displaySiteInfo(self):
         print(f"Target URL: {self.driver.current_url}")
