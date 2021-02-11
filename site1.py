64be3fc125b401cede12ee4753a593d8db75f4c8
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 09:44:44 +0800

    Update

--- site1.py
+++ site1.py
@@ -5,8 +5,7 @@ class Site1(WebDriver):
     pass
 
 site1= Site1("https://furu-po.com/")
+site1.displaySiteInfo()
 site1.driver.get(site1.url)
-site1.driver.current_url
 # elementTag is based on the <ul> before the <li>
-
 listcategory = site1.categoryFinder(html= site1.driver.page_source, elementTag = "popover")
