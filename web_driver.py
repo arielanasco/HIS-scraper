927962ea31669335dc39829d685c15d3c2f97193
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 09:54:24 +0800

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -10,14 +10,17 @@ from random import sample
 
 
 class WebDriver:
-    self.userAgentList = [
+
+    def __init__(self,url):
+        self.url = url
+        self.userAgentList = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
         ]
-    self.options = Options()
-    self.options.add_argument('--no-sandbox')
-    self.options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images":2,
+        self.options = Options()
+        self.options.add_argument('--no-sandbox')
+        self.options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images":2,
                                             "profile.default_content_setting_values.notifications":2,
                                             "profile.managed_default_content_settings.stylesheets":2,
                                             "profile.managed_default_content_settings.cookies":2,
@@ -28,11 +31,9 @@ class WebDriver:
                                             "profile.managed_default_content_settings.media_stream":2,
                                             })
 
-    self.options.add_argument("--headless")
-    self.options.add_argument(f'--user-agent="{sample(self.userAgentList,1)[0]}"')
-    self.warnings.filterwarnings('ignore')
-    def __init__(self,url):
-        self.url = url
+        self.options.add_argument("--headless")
+        self.options.add_argument(f'--user-agent="{sample(self.userAgentList,1)[0]}"')
+        self.warnings.filterwarnings('ignore')
         self.driver = webdriver.Chrome(options=self.options)
 
     def displaySiteInfo(self):
