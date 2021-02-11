3560cd3f9ddb6c9eacb530e72b2baf686219b1cb
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 09:29:40 +0800

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -5,12 +5,15 @@ from time import sleep
 from bs4 import BeautifulSoup as bs
 import re
 import pandas as pd
-import random
+from random import sample 
+
+
+
 class WebDriver:
     userAgentList = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+        ""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"",
+        ""Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"",
+        ""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393""
         ]
     options = Options()
     options.add_argument('--no-sandbox')
