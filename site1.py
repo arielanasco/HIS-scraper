7e00bba661e30993ae41484520f5134e264c7dcd
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 14:39:13 +0800

    Update site1.py

--- site1.py
+++ site1.py
@@ -21,7 +21,7 @@ for data in site1.categoryParser(html= site1.driver.page_source, elementTag = "p
             else:
                 html = bs(site1.driver.page_source, 'html.parser')
                 nextTag = html.find(class_ = "next")
-                    if not nextTag.a:
+                    if !nextTag.a:
                         print("No next page")
                         break
         else:
