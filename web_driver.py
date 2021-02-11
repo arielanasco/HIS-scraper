c43a4e2a1c896fa3c71037e256b1acaa6f23b2b8
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 13:57:16 +0800

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -94,7 +94,7 @@ class WebDriver:
         self.container = self.html.find(class_=self.elementContainer)
         self.ChildElement = self.container.find_next()
         while True:
-            self.itemList.append(self.ChildElement.find("a").get("href"))
+            self.itemList.append([self.ChildElement.find("a").get("href"),self.collector[0][1]])
             if self.ChildElement.find_next_sibling():
                 self.ChildElement = self.ChildElement.find_next_sibling()
             else:
