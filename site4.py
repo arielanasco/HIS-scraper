90f311fb577d7cb4c0be2968ac64637c3492cbbe
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-11 10:30:59 +0800

    Update site4.py

--- site4.py
+++ site4.py
@@ -3,10 +3,26 @@
 from web_driver import WebDriver
 
 class Site4(WebDriver):
-    pass
+    def categoryFinder(self,**kwargs):
+        self.collector = []
+        self.elementTag = kwargs.get("elementTag")
+        self.html = bs(kwargs.get("html"), 'html.parser')
+        self.container = self.html.find(class_="category_list01")
+        self.category = self.container.find(class_=self.elementTag)
+        self.liTag = self.category.li
+        while True:
+            # self.categoryData = re.sub(r'\([0-9]*\)', '', self.liTag.find("a").get_text())
+            self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
+            self.categoryData = re.sub(r'\W+', '', self.categoryData)
+            self.collector.append([self.liTag.find("a").get("href"),self.categoryData])
+            if self.liTag.find_next_sibling():
+                self.liTag = self.liTag.find_next_sibling()
+            else:
+                break
+        return self.collector
 
 site4= Site4("https://furusato-izumisano.jp/items/ranking.php")
-site4.driver.get(site3.url)
+site4.driver.get(site4.url)
 site4.displaySiteInfo()
 listcategory = site4.categoryFinder(html= site4.driver.page_source, elementTag = "slide-child")
 site4.driver.close()
