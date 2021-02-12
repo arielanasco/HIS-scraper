cdac23f175f94b3635b938407de369f291c25210
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-12 10:04:51 +0800

    Update site1.py

--- site1.py
+++ site1.py
@@ -1,6 +1,7 @@
 # This is the Scraper code for https://furu-po.com/ website
 from web_driver import WebDriver
 from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 class Site1(WebDriver):
     pass
@@ -13,6 +14,12 @@ categorylist = site1.categoryParser(html= site1.driver.page_source, elementTag =
 for data in categorylist:
     print(f"Scraping....{data[0]}")
     site1.driver.get(data[0])
-    dataResult = site1.listParser(html = site1.driver.page_source, elementContainer = "itemlist", category=data[1],dataResult = dataResult)
+    while True:
+        ataResult = site1.listParser(html = site1.driver.page_source, elementContainer = "itemlist", category=data[1],dataResult = dataResult)
+        if site1.driver.find_element_by_xpath("//*[@id='form_events']/section/div[2]/div[1]/div/div[2]/div[3]/ul/li[3]/a").send_keys(Keys.ENTER):
+            print(f"Scraping {site1.driver.current_url}")
+        else:
+            break
+            print(f"Done scraping for category {data[1]}")
 for _ in dataResult:
     print(_)
