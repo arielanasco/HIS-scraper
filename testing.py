eb3b16991e96faa76272787d0bd2b4bd8563b07b
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-17 14:02:53 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,28 +6,28 @@ class Site1(WebDriver):
    isActive = False
    data = []
 
-   def __init__(self,url):
-      super().__init__(url)
-   def Test(self, url_category):
-      crawler = WebDriver.Crawler(url_category)
-      crawler.driver.get(url_category)
-      print(crawler.driver.current_url)
+   # def __init__(self,url):
+   #    super().__init__(url)
+   # def Test(self, url_category):
+   #    crawler = WebDriver.Crawler(url_category)
+   #    crawler.driver.get(url_category)
+   #    print(crawler.driver.current_url)
       
 site1= Site1("https://furu-po.com/")
 site1.driver.get(site1.url)
 site1.displaySiteInfo()
 
-finish = time.perf_counter()
-for _ in categorylist:
-   print(_)
-print(f"Took {round((finish-start),2)} to complete the script")
+# finish = time.perf_counter()
+# for _ in categorylist:
+#    print(_)
+# print(f"Took {round((finish-start),2)} to complete the script")
 
-url = ["https://furu-po.com/","https://gooogle.com","https://mifurusato.jp/item_list.html"]
+# url = ["https://furu-po.com/","https://gooogle.com","https://mifurusato.jp/item_list.html"]
 
-t1 = threading.Thread(target = site1.Test ,args=(url[0],))
-t2 = threading.Thread(target = site1.Test ,args=(url[1],))
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-print("Successed!")
+# t1 = threading.Thread(target = site1.Test ,args=(url[0],))
+# t2 = threading.Thread(target = site1.Test ,args=(url[1],))
+# t1.start()
+# t2.start()
+# t1.join()
+# t2.join()
+# print("Successed!")
