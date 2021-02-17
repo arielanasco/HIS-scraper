9c33c90df620d7eb958a03e9deb96ac751025690
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-17 11:14:11 +0800

    Update

--- testing.py
+++ testing.py
@@ -1,9 +1,17 @@
 from web_driver import WebDriver
 import time
-
+import threading
 start = time.perf_counter()
 class Site1(WebDriver):
-   pass
+   isActive = False
+   data = []
+
+   def __init__(self,url):
+      super().__init__(url)
+   def Test(self, url_category):
+      self.driver.get("https://furu-po.com/")
+      print(self.driver.current_url)
+      
 site1= Site1("https://furu-po.com/")
 site1.driver.get(site1.url)
 site1.displaySiteInfo()
@@ -12,5 +20,14 @@ categorylist = site1.categoryParser(html= site1.driver.page_source, elementTag =
 finish = time.perf_counter()
 for _ in categorylist:
    print(_)
+print(f"Took {round((finish-start),2)} to complete the script")
 
-print(f"Took {finish-start} to complete the script")
+url = ["https://furu-po.com/","https://gooogle.com","https://mifurusato.jp/item_list.html"]
+
+t1 = threading.Thread(target = site1.Test ,args=(url[0],))
+t2 = threading.Thread(target = site1.Test ,args=(url[1],))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print("Successed!")
