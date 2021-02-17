9dd022a5ae3721db526a5d29f03dc7bd267877a5
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-17 15:22:00 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,10 +1,10 @@
-from web_driver import Scraper
+from web_driver import ScraperCategory
 import time
 import threading
 
 def main():
    start = time.perf_counter()
-   site1= Scraper("https://furu-po.com/")
+   site1= ScraperCategory("https://furu-po.com/")
    site1.driver.get(site1.url)
    site1.displaySiteInfo()
    final = time.perf_counter()
