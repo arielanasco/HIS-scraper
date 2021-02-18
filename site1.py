ab438a4de1f8d0843fee2c8fb3d1f3b1ec91bb0a
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-18 09:13:42 +0800

    Update

--- site1.py
+++ site1.py
@@ -22,8 +22,6 @@ for data in categorylist:
     # else:
     #     print("Already scrolled down")
     # while True:
-    #     element_present = EC.presence_of_element_located((By.CLASS_NAME, "itemlist"))
-    #     WebDriverWait(site1.driver, 3).until(element_present)
     #     dataResult = site1.listParser(html = site1.driver.page_source, elementContainer = "itemlist", category=data[1],dataResult = dataResult)
     #     try: 
     #         nextButton = site1.driver.find_element_by_xpath("//*[@id='form_events']/section/div[2]/div[1]/div/div[2]/div[3]/ul/li[3]/a")
@@ -35,7 +33,12 @@ for data in categorylist:
     # print(f"Collected URL: {len(dataResult)}")
 site1.driver.close()
 
-
+try:
+    itemlist = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "itemlist"))
+    )
+finally:
+    driver.quit()
 # def main:      
 #    site1= Site1("https://furu-po.com/")
 #    site1.driver.get(site1.url)
