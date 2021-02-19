db8214e4a02eb5c362154049bb5e82e36a9fef35
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-19 11:49:44 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -13,10 +13,10 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s](%(levelname)s@%(me
 logger = logging.getLogger(__name__)
 
 
-def ItemCollector(url_category):
+def ItemCollector(url_category,category):
    scrapeURL = ScraperList(url_category)
    scrapeURL.driver.get(scrapeURL.url)
-   logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - Scraping...")
+   logging.info(f"{threading.current_thread().name}_{category}) - Scraping...")
    while True:
       try:
          time.sleep(3)
@@ -27,13 +27,13 @@ def ItemCollector(url_category):
          try: 
             nextButton = scrapeURL.driver.find_element_by_xpath("//*[@id='form_events']/section/div[2]/div[1]/div/div[2]/div[3]/ul/li[3]/a")
             nextButton.send_keys(Keys.ENTER)
-            logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - PLS WAIT...  ACTIVE THREAD: {int(threading.activeCount())-1}")
+            logging.info(f"{threading.current_thread().name}_{category}) - PLS WAIT...  ACTIVE THREAD: {int(threading.activeCount())-1}")
          except NoSuchElementException:
-            logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - EXITING... ACTIVE THREAD: {int(threading.activeCount())-1}")
+            logging.info(f"{threading.current_thread().name}_{category}) - EXITING... ACTIVE THREAD: {int(threading.activeCount())-1}")
             while True:
                if scrapeURL.isNotActive:            
                   scrapeURL.isNotActive = False
-                  logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - Saving {len(scrapeURL.itemList)} --> {ScraperList.data}")
+                  logging.info(f"{threading.current_thread().name}_{category}) - Saving {len(scrapeURL.itemList)} --> {ScraperList.data}")
                   for _ in scrapeURL.itemList:
                      scrapeURL.data.append(_)
                   scrapeURL.isNotActive = True
@@ -58,9 +58,9 @@ def main():
    logging.info(f"{threading.current_thread().name}) - Took {round((final-start),2)} second(s) to fetch list of category")
    url = ["https://furu-po.com/goods_list/176","https://furu-po.com/goods_list/1150"]
 
-   with concurrent.futures.ThreadPoolExecutor(thread_name_prefix='Scraper') as executor:
+   with concurrent.futures.ThreadPoolExecutor(max_workers=5 , thread_name_prefix='Scraper') as executor:
       for data in site1.categoryList:
-         executor.submit(ItemCollector, (data[0]))
+         executor.submit(ItemCollector, (data[0],data[1]))
    final = time.perf_counter()
    logging.info(f"{threading.current_thread().name}) - Took {round((final-start),2)} second(s) to complete scraping category url")
 
