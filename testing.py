4c54af7a4b9f794352a7a7d8d96d520d3874a92a
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-19 11:56:46 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -56,11 +56,11 @@ def main():
    url = ["https://furu-po.com/goods_list/176","https://furu-po.com/goods_list/1150"]
 
    with concurrent.futures.ThreadPoolExecutor(max_workers=4 , thread_name_prefix='Scraper') as executor:
-      future = []
+      futures = []
       for data in site1.categoryList:
-         future.append(executor.submit(ItemCollector, (data[0],data[1])))
-      for _ in future:
-         print(_)
+         futures.append(executor.submit(ItemCollector, (data[0],data[1])))
+      for future in futures:
+         print(future.result())
    final = time.perf_counter()
    logging.info(f"{threading.current_thread().name}) - Took {round((final-start),2)} second(s)")
 
