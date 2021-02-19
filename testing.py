441f80a6e2412624d05649ed8caec12c8a33f0ab
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-19 11:45:36 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 def ItemCollector(url_category):
    scrapeURL = ScraperList(url_category)
    scrapeURL.driver.get(scrapeURL.url)
-   logging.info(f"{threading.current_thread().name}_scrapeURL.driver.title) - Scraping...")
+   logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - Scraping...")
    while True:
       try:
          time.sleep(3)
@@ -27,13 +27,13 @@ def ItemCollector(url_category):
          try: 
             nextButton = scrapeURL.driver.find_element_by_xpath("//*[@id='form_events']/section/div[2]/div[1]/div/div[2]/div[3]/ul/li[3]/a")
             nextButton.send_keys(Keys.ENTER)
-            logging.info(f"{threading.current_thread().name}_scrapeURL.driver.title) - PLS WAIT...  ACTIVE THREAD: {int(threading.activeCount())-1}")
+            logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - PLS WAIT...  ACTIVE THREAD: {int(threading.activeCount())-1}")
          except NoSuchElementException:
-            logging.info(f"{threading.current_thread().name}_scrapeURL.driver.title) - EXITING... ACTIVE THREAD: {int(threading.activeCount())-1}")
+            logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - EXITING... ACTIVE THREAD: {int(threading.activeCount())-1}")
             while True:
                if scrapeURL.isNotActive:            
                   scrapeURL.isNotActive = False
-                  logging.info(f"{threading.current_thread().name}_scrapeURL.driver.title) - Saving {len(scrapeURL.itemList)} --> {ScraperList.data}")
+                  logging.info(f"{threading.current_thread().name}_{scrapeURL.driver.title}) - Saving {len(scrapeURL.itemList)} --> {ScraperList.data}")
                   for _ in scrapeURL.itemList:
                      scrapeURL.data.append(_)
                   scrapeURL.isNotActive = True
