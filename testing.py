431369b7fe1929edf0466ad96c030ac2b0d80a9a
Ariel Anasco <ariel@linkmind.co.jp>
2021-02-23 14:10:14 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -144,13 +144,13 @@ def main():
 
    with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Scraper') as executor:
       #executor.map(URLCollector, data)
-      executor.submit(URLCollector ,data=["https://furu-po.com/goods_list/1150","test"])
+      executor.submit(URLCollector ,data=[["https://furu-po.com/goods_list/1150","test"]])
 
    final = time.perf_counter()
    logging.info(f"{threading.current_thread().name}) - Took {round((final-start),2)} second(s)")
 
-   with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Fetch') as executor:
-      executor.map(DataCollector, ScraperList.data)
+   # with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Fetch') as executor:
+   #    executor.map(DataCollector, ScraperList.data)
 
    final = time.perf_counter()
    logging.info(f"{threading.current_thread().name}) - Took {round((final-start),2)} second(s)")
