63d77d8a75bdf5bae991fcbd6493f6501979ace6
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-01 15:05:28 +0800

    Update

--- site2.py
+++ site2.py
@@ -1,14 +1,52 @@
 # This is the Scraper code for https://www.satofull.jp/ website
+from web_driver import ScraperCategory,ScraperList
+import time
+import threading
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import logging
+import  concurrent.futures
+from bs4 import BeautifulSoup as bs
+import re
 
-from web_driver import WebDriver
+class ScraperCategory_(WebDriver):
+    categoryList = []
 
-class Site2(WebDriver):
-    pass
+    def __init__(self, url):
+        self.url = url
+        super().__init__()
 
-site2= Site2("https://www.satofull.jp/")
-site2.driver.get(site2.url)
-site2.displaySiteInfo()
-listcategory = site2.categoryParser(html= site2.driver.page_source, elementTag = "SideBox__list--item")
-for data in listcategory:
-    print(f"{data[0]} {data[1]}")
-site2.driver.close()
+    def categoryParser(self,**kwargs):
+        self.elementTag = kwargs.get("elementTag")
+        self.html = bs(kwargs.get("html"), 'html.parser')
+        self.category = self.html.(class_=self.elementTag).find(class_="SideBox__tree")
+        self.liTag = self.category.li
+        while True:
+            self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
+            self.categoryData = re.sub(r'\W+', '', self.categoryData)
+            ScraperCategory.categoryList.append([self.liTag.find("a").get("href"),self.categoryData])
+            if self.liTag.find_next_sibling():
+                self.liTag = self.liTag.find_next_sibling()
+            else:
+                break
+
+def main():
+   start = time.perf_counter()
+   logging.info(f"{threading.current_thread().name}) - Scraping has been started...")
+   site2= ScraperCategory("https://www.satofull.jp/products/list.php?cnt=60&p=1")
+   site2.driver.get(site2.url)
+   current_url, user_agent = site2.displaySiteInfo()
+   logging.info(f"{threading.current_thread().name}) - {current_url} {user_agent}")
+   site2.categoryParser(html= site2.driver.page_source, elementTag = "Section")
+   data=site2.categoryList
+   site2.driver.close()
+
+   print(data)
+
+
+   
+if __name__ == '__main__':
+   main()
