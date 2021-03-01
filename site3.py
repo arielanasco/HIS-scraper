efc6a6ca4671a794fa1eb38788b7620b8ba4b76e
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-01 15:49:55 +0800

    Update site3.py

--- site3.py
+++ site3.py
@@ -12,6 +12,28 @@ import  concurrent.futures
 from bs4 import BeautifulSoup as bs
 import re
 
+class ScraperCategory_(ScraperCategory):
+    categoryList = []
+
+    def __init__(self, url):
+        self.url = url
+        super().__init__()
+
+    def categoryParser(self,**kwargs):
+        self.elementTag = kwargs.get("elementTag")
+        self.html = bs(kwargs.get("html"), 'html.parser')
+        self.category = self.html.find(class_=self.elementTag)
+        self.category = self.category.find(class_="dropdownlist")
+        self.liTag = self.category.li
+        while True:
+            self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
+            self.categoryData = re.sub(r'\W+', '', self.categoryData)
+            ScraperCategory.categoryList.append([self.liTag.find("a").get("href"),self.categoryData])
+            if self.liTag.find_next_sibling():
+                self.liTag = self.liTag.find_next_sibling()
+            else:
+                break
+
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s](%(levelname)s@%(message)s', datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
@@ -20,7 +42,7 @@ logger = logging.getLogger(__name__)
 def main():
    start = time.perf_counter()
    logging.info(f"{threading.current_thread().name}) - Scraping has been started...")
-   site3= ScraperCategory("https://tokyu-furusato.jp/goods/result")
+   site3= ScraperCategory_("https://tokyu-furusato.jp/goods/result")
    site3.driver.get(site3.url)
    current_url, user_agent = site3.displaySiteInfo()
    logging.info(f"{threading.current_thread().name}) - {current_url} {user_agent}")
