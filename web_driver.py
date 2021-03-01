f5e8c0fca12206734aeb5b82fc7e81d0de0b1f9f
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-01 17:21:33 +0800

    Update

--- web_driver.py
+++ web_driver.py
@@ -5,14 +5,14 @@ import warnings
 from time import sleep
 from bs4 import BeautifulSoup as bs
 import re
-import pandas as pd
 from random import sample 
 
 
 
 class WebDriver:
     warnings.filterwarnings('ignore')
-    def __init__(self):
+    def __init__(self,url):
+        self.url = url
         self.userAgentList = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
@@ -36,47 +36,55 @@ class WebDriver:
         self.options.add_argument(f'--user-agent="{sample(self.userAgentList,1)[0]}"')
         self.driver = webdriver.Chrome(options=self.options)
 
+    def get(self):
+        return self.driver.get(self.url)
+
+    def get_page_source(self):
+        return self.driver.page_source(self.url)
+
     def displaySiteInfo(self):
         return f"Target URL: {self.driver.current_url}" , f"User-Agent: {self.driver.execute_script('return navigator.userAgent;')}"
+
     
-class ScraperCategory(WebDriver):
-    categoryList = []
+    
+# class ScraperCategory(WebDriver):
+#     categoryList = []
 
-    def __init__(self, url):
-        self.url = url
-        super().__init__()
+#     def __init__(self, url):
+#         self.url = url
+#         super().__init__()
 
-    def categoryParser(self,**kwargs):
-        self.elementTag = kwargs.get("elementTag")
-        self.html = bs(kwargs.get("html"), 'html.parser')
-        self.category = self.html.find(class_=self.elementTag)
-        self.liTag = self.category.li
-        while True:
-            self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
-            self.categoryData = re.sub(r'\W+', '', self.categoryData)
-            ScraperCategory.categoryList.append([self.liTag.find("a").get("href"),self.categoryData])
-            if self.liTag.find_next_sibling():
-                self.liTag = self.liTag.find_next_sibling()
-            else:
-                break
+#     def categoryParser(self,**kwargs):
+#         self.elementTag = kwargs.get("elementTag")
+#         self.html = bs(kwargs.get("html"), 'html.parser')
+#         self.category = self.html.find(class_=self.elementTag)
+#         self.liTag = self.category.li
+#         while True:
+#             self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
+#             self.categoryData = re.sub(r'\W+', '', self.categoryData)
+#             ScraperCategory.categoryList.append([self.liTag.find("a").get("href"),self.categoryData])
+#             if self.liTag.find_next_sibling():
+#                 self.liTag = self.liTag.find_next_sibling()
+#             else:
+#                 break
 
-class ScraperList(WebDriver):
-    isNotActive = True
-    data = []
+# class ScraperList(WebDriver):
+#     isNotActive = True
+#     data = []
 
-    def __init__(self, url):
-        self.url = url
-        self.itemList = []
-        super().__init__()
+#     def __init__(self, url):
+#         self.url = url
+#         self.itemList = []
+#         super().__init__()
 
-    def listParser(self,html,elementContainer):
-        self.elementContainer = elementContainer
-        self.html = bs(html, 'html.parser')
-        self.container = self.html.find(class_=self.elementContainer)
-        self.ChildElement = self.container.find_next()
-        while True:
-            self.itemList.append(self.ChildElement.find("a").get("href"))
-            if self.ChildElement.find_next_sibling():
-                self.ChildElement = self.ChildElement.find_next_sibling()
-            else:
-                break
+#     def listParser(self,html,elementContainer):
+#         self.elementContainer = elementContainer
+#         self.html = bs(html, 'html.parser')
+#         self.container = self.html.find(class_=self.elementContainer)
+#         self.ChildElement = self.container.find_next()
+#         while True:
+#             self.itemList.append(self.ChildElement.find("a").get("href"))
+#             if self.ChildElement.find_next_sibling():
+#                 self.ChildElement = self.ChildElement.find_next_sibling()
+#             else:
+#                 break
