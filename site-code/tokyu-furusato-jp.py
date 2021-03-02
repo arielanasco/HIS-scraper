0bd5e87fe24887ed916a41eac5ef98d0662d33a5
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-02 10:00:48 +0800

    Update

--- site-code/tokyu-furusato-jp.py
+++ site-code/tokyu-furusato-jp.py
@@ -4,8 +4,7 @@ Site : Hometown pallet
 Link : https://tokyu-furusato.jp/
 
 """
-
-from web_driver import ScraperCategory,ScraperList
+from web_driver import WebDriver
 import time
 import threading
 from selenium.webdriver.common.by import By
@@ -17,8 +16,14 @@ import logging
 import  concurrent.futures
 from bs4 import BeautifulSoup as bs
 import re
+""" This section declares all the variables used """
+LINK = "https://tokyu-furusato.jp/"
 
-class ScraperCategory_(ScraperCategory):
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s](%(levelname)s@%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+
+class ScraperCategory(WebDriver):
     categoryList = []
 
     def __init__(self, url):
@@ -28,8 +33,8 @@ class ScraperCategory_(ScraperCategory):
     def categoryParser(self,**kwargs):
         self.elementTag = kwargs.get("elementTag")
         self.html = bs(kwargs.get("html"), 'html.parser')
-        self.category = self.html.find(class_=self.elementTag)
-        self.category = self.category.find(class_="dropdownlist")
+        self.category = self.html.find(class_="section_category")
+        self.category = self.category.find(class_=self.elementTag)
         self.liTag = self.category.li
         while True:
             self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
@@ -40,25 +45,39 @@ class ScraperCategory_(ScraperCategory):
             else:
                 break
 
+class ScraperList(WebDriver):
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s](%(levelname)s@%(message)s', datefmt='%d-%b-%y %H:%M:%S')
-logger = logging.getLogger(__name__)
+    isNotActive = True
+    data = []
+
+    def __init__(self, url):
+        self.url = url
+        self.itemList = []
+        super().__init__()
+
+    def listParser(self,html,elementContainer):
+        self.elementContainer = elementContainer
+        self.html = bs(html, 'html.parser')
+        self.container = self.html.find(class_=self.elementContainer)
+        self.ChildElement = self.container.find_next()
+        while True:
+            self.itemList.append(self.ChildElement.find("a").get("href"))
+            if self.ChildElement.find_next_sibling():
+                self.ChildElement = self.ChildElement.find_next_sibling()
+            else:
+                break
 
 
-def main():
-   start = time.perf_counter()
-   logging.info(f"{threading.current_thread().name}) - Scraping has been started...")
-   site3= ScraperCategory_("https://tokyu-furusato.jp/goods/result")
-   site3.driver.get(site3.url)
-   current_url, user_agent = site3.displaySiteInfo()
-   logging.info(f"{threading.current_thread().name}) - {current_url} {user_agent}")
-   site3.categoryParser(html= site3.driver.page_source, elementTag = "section_localnav")
-   data=site3.categoryList
-   site3.driver.close()
-
-   print(data)
-
-
-   
 if __name__ == '__main__':
-   main()
+    start = time.perf_counter()
+    logging.info(f"{threading.current_thread().name}) - Scraping has been started...")
+    site=ScraperCategory(LINK)
+    site.get()
+    current_url, user_agent = site.displaySiteInfo()
+    logging.info(f"{threading.current_thread().name}) - {current_url} {user_agent}")
+    site.categoryParser(html= site.driver.page_source, elementTag ="categorylist")
+    data=site.categoryList
+    site.driver.close()
+    final = time.perf_counter()
+    logging.info(f"{threading.current_thread().name}) - Took {round((final-start),2)} for fetching categories")
+    print(data)
