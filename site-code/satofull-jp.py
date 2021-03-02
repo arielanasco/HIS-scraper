595b38b8977b4db91e9cba9f07b53f1c0b3468da
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-02 09:17:48 +0800

    Update satofull-jp.py

--- site-code/satofull-jp.py
+++ site-code/satofull-jp.py
@@ -33,7 +33,7 @@ class ScraperCategory(WebDriver):
     def categoryParser(self,**kwargs):
         self.elementTag = kwargs.get("elementTag")
         self.html = bs(kwargs.get("html"), 'html.parser')
-        self.category = self.category.find(class_=self.elementTag)
+        self.category = self.html.find(class_=self.elementTag)
         self.liTag = self.category.li
         while True:
             self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
