5c47d1b5c489630f3779f448c2fe1030e540488c
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-03 10:16:51 +0800

    Update furusato-tax-jp.py

--- site-code/furusato-tax-jp.py
+++ site-code/furusato-tax-jp.py
@@ -166,12 +166,11 @@ if __name__ == '__main__':
     site.driver.close()
     final = time.perf_counter()
     logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds for fetching categories")
-    print(data)
-    # start = time.perf_counter()
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Scraper') as executor:
-    #     futures = [executor.submit(DataCollectorFunction, data) for data in datum]
-    #     for future in concurrent.futures.as_completed(futures):
-    #         if future.result():
-    #             print(future.result())
-    # final = time.perf_counter()
-    # logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(DataCollector.data)} items URL")
+    start = time.perf_counter()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Scraper') as executor:
+        futures = [executor.submit(DataCollectorFunction, data) for data in datum]
+        for future in concurrent.futures.as_completed(futures):
+            if future.result():
+                print(future.result())
+    final = time.perf_counter()
+    logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(DataCollector.data)} items URL")
