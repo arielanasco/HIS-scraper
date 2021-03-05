8df99df03b66b324c9bfd6aaa68d38468189d57d
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-05 11:10:22 +0800

    Update furupo-com.py

--- site-code/furupo-com.py
+++ site-code/furupo-com.py
@@ -124,7 +124,7 @@ def DataCollectorFunction(data):
     logging.info(f"{threading.current_thread().name}) - Fetching...{item_url}")
     try:
         time.sleep(1)
-        item_info = WebDriverWait(scrapeURL.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "lg-info")))
+        item_info = WebDriverWait(scrapeURL.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "item_detail")))
         scrapeURL.dataParser(html = scrapeURL.driver.page_source,
                            itemUrl = item_url, 
                            localNameFinder = "lg-info",
