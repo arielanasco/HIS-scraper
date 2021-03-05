e95cd1adaef2adda99121b0393ac4e1ca72faea5
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-05 10:47:41 +0800

    Update furusato-tax-jp.py

--- site-code/furusato-tax-jp.py
+++ site-code/furusato-tax-jp.py
@@ -100,6 +100,7 @@ class DataCollector(WebDriver):
             self.imageUrlFinder = self.html.find(id=imageUrlFinder).find(class_="sld__list").find_all("li")
             self.imageList = []
             for _ in self.imageUrlFinder:
+                logging.info(f"{threading.current_thread().name}) -{_}")
                 self.imageList.append(_.get("src")) 
         except:
             raise Exception ("Unable to locate the imageUrlFinder")
