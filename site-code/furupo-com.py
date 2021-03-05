270ffb6d6e8002286d9edf66ba84e66bf98c10c1
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-05 11:32:54 +0800

    Update furupo-com.py

--- site-code/furupo-com.py
+++ site-code/furupo-com.py
@@ -163,7 +163,7 @@ def ItemLinkCollector(data):
                     if scrapeURL.isNotActive:            
                         scrapeURL.isNotActive = False
                         for _ in scrapeURL.itemList:
-                            scrapeURL.data.append([LINK+_,category])
+                            scrapeURL.data.append([_,category])
                         scrapeURL.isNotActive = True
                         logging.info(f"{threading.current_thread().name}) -Adding {len(scrapeURL.itemList)} items | Total item {len(scrapeURL.data)}")
                         break
