cab04d0e0d727341fc6a329255f88ccdbe5568f3
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-09 11:16:32 +0800

    Update furusato-ana-co-jp.py

--- site-code/furusato-ana-co-jp.py
+++ site-code/furusato-ana-co-jp.py
@@ -8,16 +8,19 @@ Link : https://furusato.ana.co.jp/
 import time
 import threading
 import requests
-
+from random import sample 
 import logging
 import  concurrent.futures
 from bs4 import BeautifulSoup as bs
 import re
 """ This section declares all the variables used """
 LINK = "https://furusato.ana.co.jp/products/list.php"
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s](%(levelname)s@%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
 class Webdriver:
-    def __init__(self,url):
-        self.url = url
+    def __init__(self):
         self.userAgentList = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
@@ -26,91 +29,152 @@ class Webdriver:
         "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0"
         ]
         self.requests = requests
+        self.headers = {"User-Agent": f"{sample(self.userAgentList,1)[0]}"}
+    
+    def get(self,url):
+        self.html = self.requests.get(url, headers = self.headers)
+        self.html.close()
+        return self.html             
 
+    def displaySiteInfo(self):
+        return f"User-Agent: {self.headers}"
 
 class ScraperCategory(Webdriver):
     categoryList = []
 
-    def __init__(self, url):
-        self.url = url
-        super().__init__(url)
-
-    def categoryParser(self,**kwargs):
-        self.elementTag = kwargs.get("elementTag")
-        self.html = bs(kwargs.get("html"), 'html.parser')
-        self.category = self.html.find(class_="search-parent-categories")
-        self.category = self.category.find(class_=self.elementTag)
-        self.liTag = self.category.li
-        while True:
-            self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
-            self.categoryData = re.sub(r'\W+', '', self.categoryData)
-            ScraperCategory.categoryList.append(["https://www.furusato-tax.jp"+self.liTag.find("a").get("href"),self.categoryData])
-            if self.liTag.find_next_sibling():
-                self.liTag = self.liTag.find_next_sibling()
-            else:
-                break
-
-
-    def __init__(self, url):
-        self.url = url
-        super().__init__(url)
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s](%(levelname)s@%(message)s', datefmt='%d-%b-%y %H:%M:%S')
-logger = logging.getLogger(__name__)
-
-
-class ScraperCategory(WebDriver):
-    categoryList = []
-
-    def __init__(self, url):
-        self.url = url
-        super().__init__(url)
+    def __init__(self):
+        super().__init__()
 
     def categoryParser(self,**kwargs):
         self.elementTag = kwargs.get("elementTag")
         self.html = bs(kwargs.get("html"), 'html.parser')
         self.category = self.html.find(class_=self.elementTag)
-        self.liTag = self.category.find_all("li")
-        for litag in self.liTag:
-            self.categoryData = re.sub(r'\([^()]*\)', '', litag.find("a").get_text())
+        self.liTag = self.category.li
+        while True:
+            self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
             self.categoryData = re.sub(r'\W+', '', self.categoryData)
-            ScraperCategory.categoryList.append([litag.find("a").get("href"),self.categoryData])
+            ScraperCategory.categoryList.append([self.liTag.find("a").get("href"),self.categoryData])
+            if self.liTag.find_next_sibling():
+                self.liTag = self.liTag.find_next_sibling()
+            else:
+                break
 
-class DataCollector(WebDriver):
+class DataCollector(Webdriver):
 
     isNotActive = True
     data = []
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self):
         self.itemList = []
-        super().__init__(url)
+        super().__init__()
 
     def listParser(self,html,elementContainer):
         self.elementContainer = elementContainer
         self.html = bs(html, 'html.parser')
-        self.container = self.html.find(class_="as-flex_left")
-        self.container = self.container.find_all(self.elementContainer)
-        for ChildElement in  self.container:
-            self.itemList.append(ChildElement.find("div").get("data-product-url"))
+        self.container = self.html.find(id="main_column")
+        self.container = self.container.find(class_=self.elementContainer)
+        self.ChildElement = self.container.find_next()
+        while True:
+            self.itemList.append(self.ChildElement.find("div").get("data-product-url"))
+            if self.ChildElement.find_next_sibling():
+                self.ChildElement = self.ChildElement.find_next_sibling()
+            else:
+                break
 
+    def dataParser(self,html,itemUrl,localNameFinder,titleFinder,descriptionFinder,priceFinder,capacityFinder,imageUrlFinder):
+        self.html = bs(html, 'html.parser')
+        logging.info(f"{threading.current_thread().name}) -Getting data now...")
+        try:
+            self.localNameFinder = self.html.find(class_=localNameFinder).get_text()
+            self.localNameFinder =  re.sub(r'\W+', '', self.localNameFinder)
+        except:
+            raise Exception ("Unable to locate the localNameFinder")
+        try:
+            self.titleFinder = self.html.find(class_=titleFinder).get_text()
+            self.titleFinder = re.sub(r'\W+', '', self.titleFinder)
+        except:
+            raise Exception ("Unable to locate the titleFinder")
+        try:
+            self.descriptionFinder = self.html.find(class_=descriptionFinder).get_text()
+            self.descriptionFinder = re.sub(r'\W+', '', self.descriptionFinder)
+        except:
+            raise Exception ("Unable to locate the descriptionFinder")
+        try:
+            self.priceFinder = self.html.find(class_=priceFinder).get_text()
+            self.priceFinder = re.sub(r'\W+', '', self.priceFinder)
+        except:
+            raise Exception ("Unable to locate the priceFinder")
+        try:
+            self.capacityFinder = self.html.find(class_=capacityFinder).get_text()
+            self.capacityFinder = re.sub(r'\W+', '', self.capacityFinder)
+        except:
+            raise Exception ("Unable to locate the capacityFinder")
+        try:
+            self.imageUrlFinder = self.html.find(id=imageUrlFinder).find(class_="sld__list").find_all("li")
+            self.imageList = []
+            for _ in self.imageUrlFinder:
+                self.imageList.append(_.find("img").get("src")) 
+        except:
+            raise Exception ("Unable to locate the imageUrlFinder")
+        while True:
+            if DataCollector.isNotActive: 
+                DataCollector.isNotActive = False
+                for data in DataCollector.data:
+                    if itemUrl in data:
+                        index_ = DataCollector.data.index(data)
+                        DataCollector.data[index_].insert(2,self.localNameFinder)
+                        DataCollector.data[index_].insert(3,self.titleFinder)
+                        DataCollector.data[index_].insert(4,self.descriptionFinder)
+                        DataCollector.data[index_].insert(5,self.priceFinder)
+                        DataCollector.data[index_].insert(6,self.capacityFinder)
+                        DataCollector.data[index_].insert(7,self.imageList)
+                        DataCollector.isNotActive = True
+                        break
+            break
 
-
+def ItemLinkCollector(data):
+    element_container = "as-flex_left"
+    url_category=data[0]
+    category=data[1]
+    scrapeURL = DataCollector()
+    logging.info(f"{threading.current_thread().name}) -Scraping...{category}:{url_category}")
+    while True:
+        html =scrapeURL.get(url_category).text
+        time.sleep(3)
+        scrapeURL.listParser(html =html, elementContainer = element_container)
+        html_ = bs(html, 'html.parser')
+        nextButton = html_.find(class_="pager_links")
+        nextButton = nextButton.find_all(class_="pager_link")
+        url_category_ = LINK+nextButton[-1].find("a").get("href")
+        if url_category != url_category_:
+            url_category = url_category_
+            logging.info(f"{threading.current_thread().name}) -Active_thread : {int(threading.activeCount())-1} Next_Page of {category}")
+        else:
+            scrapeURL.listParser(html =html, elementContainer = element_container)
+            while True:
+                if scrapeURL.isNotActive:            
+                    scrapeURL.isNotActive = False
+                    for _ in scrapeURL.itemList:
+                        scrapeURL.data.append([LINK+_,category])
+                    scrapeURL.isNotActive = True
+                    logging.info(f"{threading.current_thread().name}) -Adding {len(scrapeURL.itemList)} items")
+                    break
+            break
 
 if __name__ == '__main__':
     start = time.perf_counter()
     logging.info(f"{threading.current_thread().name}) -Scraping has been started...")
-    site=ScraperCategory(LINK)
-    site.get()
-    current_url, user_agent = site.displaySiteInfo()
-    logging.info(f"{threading.current_thread().name}) -{current_url} {user_agent}")
-    site.categoryParser(html= site.driver.page_source, elementTag = "link_wrap")
-    datum=site.categoryList
-    site.driver.close()
+    site=ScraperCategory()
+    user_agent = site.displaySiteInfo()
+    logging.info(f"{threading.current_thread().name}) -{user_agent}")
+    site.categoryParser(html= site.get(LINK).text, elementTag = "link_wrap")
+    # datum=site.categoryList
+    datum = [['https://furusato.ana.co.jp/products/list.php?s4=ANA%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB&sort=number5%2CNumber1%2CScore','test']]
     final = time.perf_counter()
     logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds for fetching {len(datum)} categories")
     start = time.perf_counter()
     with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Scraper') as executor:
-        futures = [executor.submit(DataCollectorFunction, data) for data in datum]
+        futures = [executor.submit(ItemLinkCollector, data) for data in datum]
         for future in concurrent.futures.as_completed(futures):
             if future.result():
                 logging.info(f"{threading.current_thread().name}) -{future.result()}")
