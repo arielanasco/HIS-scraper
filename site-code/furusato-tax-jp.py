178c578f3c17b43ed9a1734734f5b6bacd9db9d0
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-10 16:54:52 +0800

    Update furusato-tax-jp.py

--- site-code/furusato-tax-jp.py
+++ site-code/furusato-tax-jp.py
@@ -197,8 +197,8 @@ if __name__ == '__main__':
     current_url, user_agent = site.displaySiteInfo()
     logging.info(f"{threading.current_thread().name}) -{current_url} {user_agent}")
     site.categoryParser(html= site.driver.page_source, elementTag = "search-child-categories")
-    # data=site.categoryList
-    data=[['https://www.furusato-tax.jp/search/152?disabled_category_top=1&target=1','test']]
+    data=site.categoryList
+    # data=[['https://www.furusato-tax.jp/search/152?disabled_category_top=1&target=1','test']]
     site.driver.close()
     final = time.perf_counter()
     logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds for fetching {len(data)} categories")
