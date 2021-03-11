ef539b26562c5780f8a1b8a41681797e292797b4
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-11 14:13:31 +0800

    Update tokyu-furusato-jp.py

--- site-code/tokyu-furusato-jp.py
+++ site-code/tokyu-furusato-jp.py
@@ -206,8 +206,8 @@ if __name__ == '__main__':
     current_url, user_agent = site.displaySiteInfo()
     logging.info(f"{threading.current_thread().name}) - {current_url} {user_agent}")
     site.categoryParser(html= site.driver.page_source, elementTag ="dropdownlist")
-    # data=site.categoryList
-    data=[['https://tokyu-furusato.jp/goods/result?chk_ctg[1]=15', 'Testing']]
+    data=site.categoryList
+    # data=[['https://tokyu-furusato.jp/goods/result?chk_ctg[1]=15', 'Testing']]
     site.driver.quit()
     final = time.perf_counter()
     logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(data)} categories")
