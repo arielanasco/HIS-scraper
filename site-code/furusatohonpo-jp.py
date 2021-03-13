4ffb87220638f83835b162876098f26898780ccf
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-13 08:09:03 +0800

    Update furusatohonpo-jp.py

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -43,7 +43,7 @@ class ScraperCategory(WebDriver):
         while True:
             self.categoryData = re.sub(r'\([^()]*\)', '', self.liTag.find("a").get_text())
             self.categoryData = re.sub(r'\W+', '', self.categoryData)
-            ScraperCategory.categoryList.append(["https://furusatohonpo.jp"+self.liTag.find("a").get("href"),self.categoryData])
+            ScraperCategory.categoryList.append({"URL":"https://furusatohonpo.jp"+self.liTag.find("a").get("href"),"category":self.categoryData})
             if self.liTag.find_next_sibling():
                 self.liTag = self.liTag.find_next_sibling()
             else:
