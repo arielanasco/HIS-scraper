f1a561f921c1fae1dcaa0fd8c4d7b635cb64a4cf
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-13 08:32:38 +0800

    Update furusatohonpo-jp.py

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
     logging.info(f"{threading.current_thread().name}) -{current_url} {user_agent}")
     site.categoryParser(html= site.driver.page_source, elementTag ="p-topCategory__list")
     data=site.categoryList
-    data=[{"URL":"https://furusatohonpo.jp/donate/s/?categories=18","category":"test"}]
+    # data=[{"URL":"https://furusatohonpo.jp/donate/s/?categories=18","category":"test"}]
     site.driver.quit()
     final = time.perf_counter()
     logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} for fetching {len(data)} categories")
