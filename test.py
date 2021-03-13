7a725b52747b7a15b47c8f793bebe761eb479175
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-13 11:10:16 +0800

    Update

--- test.py
+++ test.py
@@ -13,34 +13,37 @@ from PIL import Image
 # for _ in img:
 #     print(_)
 
+import logging
+logging.basicConfig(format='[%(asctime)s](%(levelname)s@%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
+logging.debug("This will get logged")
 
 
-
-cwd = os.getcwd()
-site_name = os.path.basename(__file__).split(".")[0]
-
-
-def save_image_to_file(response,category,product_name):
-    dirname= os.path.join(cwd,site_name,product_name)
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    dirFile = os.path.join(dirname,product_name)
-    print (dirFile)
-    with open(dirFile, 'wb') as out_file:
-        shutil.copyfileobj(response.raw, out_file)
-        img = Image.open(dirFile)    
-        print (img.size)
-
-def download_images(link, category,product_name):
-    print ("Downloading...")
-    response = requests.get(link, stream=True)
-    save_image_to_file(response,category,product_name)
-    del response
-
-download_images("https://img.furusato-tax.jp/cdn-cgi/image/width=520,height=323/img/x/product/details/20200713/sd1_0e40bfca39974cbbf35002453f8d4e91e7ed22a0.jpg","fruits","img_1.jpg")
-print(site_name)
-
-site-name/category/item-data/imgaes_list
+# cwd = os.getcwd()
+# site_name = os.path.basename(__file__).split(".")[0]
 
 
-filename = os.path.join('test\testing1\img_1.jpg')
+# def save_image_to_file(response,category,product_name):
+#     dirname= os.path.join(cwd,site_name,product_name)
+#     if not os.path.exists(dirname):
+#         os.makedirs(dirname)
+#     dirFile = os.path.join(dirname,product_name)
+#     print (dirFile)
+#     with open(dirFile, 'wb') as out_file:
+#         shutil.copyfileobj(response.raw, out_file)
+#         img = Image.open(dirFile)    
+#         print (img.size)
+
+# def download_images(link, category,product_name):
+#     print ("Downloading...")
+#     response = requests.get(link, stream=True)
+#     save_image_to_file(response,category,product_name)
+#     del response
+
+# download_images("https://img.furusato-tax.jp/cdn-cgi/image/width=520,height=323/img/x/product/details/20200713/sd1_0e40bfca39974cbbf35002453f8d4e91e7ed22a0.jpg","fruits","img_1.jpg")
+# print(site_name)
+
+# site-name/category/item-data/imgaes_list
+
+
+# filename = os.path.join('test\testing1\img_1.jpg')
