6974c8f785c65f64490d1a9fc7801ab4a485d24b
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-15 08:03:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -25,31 +25,25 @@ for _ in DataParserClass.data:
     else:
         print(f"{DataParserClass.data.index(_)}  {_['URL']} Detected bug")
 
-# cwd = os.getcwd()
-# site_name = os.path.basename(__file__).split(".")[0]
+cwd = os.getcwd()
+site_name = os.path.basename(__file__).split(".")[0]
 
 
-# def save_image_to_file(response,category,product_name):
-#     dirname= os.path.join(cwd,site_name,product_name)
-#     if not os.path.exists(dirname):
-#         os.makedirs(dirname)
-#     dirFile = os.path.join(dirname,product_name)
-#     print (dirFile)
-#     with open(dirFile, 'wb') as out_file:
-#         shutil.copyfileobj(response.raw, out_file)
-#         img = Image.open(dirFile)    
-#         print (img.size)
+def save_image_to_file(self,images):
+    for image in images:
+        response = requests.get(image, stream=True)
+        dirname= os.path.join(cwd,'scraper',site_name)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        dirFile = os.path.join(dirname,product_name)
+        with open(dirFile, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+            img = Image.open(dirFile)    
+        del response
 
-# def download_images(link, category,product_name):
-#     print ("Downloading...")
-#     response = requests.get(link, stream=True)
-#     save_image_to_file(response,category,product_name)
-#     del response
+def download_images(link, category,product_name):
+    print ("Downloading...")
+    response = requests.get(link, stream=True)
+    save_image_to_file(response,category,product_name)
 
-# download_images("https://img.furusato-tax.jp/cdn-cgi/image/width=520,height=323/img/x/product/details/20200713/sd1_0e40bfca39974cbbf35002453f8d4e91e7ed22a0.jpg","fruits","img_1.jpg")
-# print(site_name)
-
-# site-name/category/item-data/imgaes_list
-
-
-# filename = os.path.join('test\testing1\img_1.jpg')
+filename = os.path.join('test\testing1\img_1.jpg')
