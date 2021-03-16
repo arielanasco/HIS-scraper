89ccc28d058f8e04683d0b886f370970b6597357
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-16 11:08:31 +0800

    Update furusatohonpo-jp.py

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -220,15 +220,12 @@ if __name__ == '__main__':
     logging.info(f"{threading.current_thread().name}) -Scraping has been started...")
     site=ScraperCategory("https://furusatohonpo.jp/donate/s/?")
     site.driver.get(site.url)
-    cat_container = site.driver.find_elements_by_css_selector("ul.p-sortNavPCCategory__listLv2")
+    cat_container = site.driver.find_elements_by_css_selector("p-sortNavPCCategory__itemLv2")
     for cat in cat_container:
-        print(cat)
-        cat_container_ = cat.find_elements_by_css_selector("p-sortNavPCCategory__itemLv2")
-        for cat_ in cat_container_:
-            cat_.find_element_by_css_selector("input").send_keys(Keys.ENTER)
-            catt  = cat_.find_element_by_css_selector("a.js-sortAccBtn").text
-            site.categoryParser(html= site.driver.current_url, elementCat =catt)
-            cat_.find_element_by_css_selector("input").send_keys(Keys.ENTER)
+        cat.find_element_by_css_selector("input").send_keys(Keys.ENTER)
+        catt  = cat.find_element_by_css_selector("a.js-sortAccBtn").text
+        site.categoryParser(html= site.driver.current_url, elementCat =catt)
+        cat.find_element_by_css_selector("input").send_keys(Keys.ENTER)
     data=site.categoryList
     # data=[{"URL":"https://furusatohonpo.jp/donate/s/?categories=18","category":"test"}]
     # {"URL":"https://furusatohonpo.jp/donate/s/?categories=1601","category":"test2"}]
