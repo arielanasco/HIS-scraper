03a0f07901d55a7f5785330043377e3148efa161
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-23 14:07:19 +0800

    Update

--- site-code/tokyu-furusato-jp.py
+++ site-code/tokyu-furusato-jp.py
@@ -260,24 +260,24 @@ site.driver.quit()
 final = time.perf_counter()
 logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(data)} categories")
 # data=[{'URL':'https://tokyu-furusato.jp/goods/result?limit=&order=1&chk_sub_ctg%5B%5D=82', 'category':'東急オリジナルお礼品'}]
-start = time.perf_counter()
-with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Fetching_URL') as executor:
-    futures = [executor.submit(ItemLinkCollector, datum) for datum in data]
-    for future in concurrent.futures.as_completed(futures):
-        if future.result():
-            logging.info(f"{threading.current_thread().name}) -{future.result()}")
-final = time.perf_counter()
-logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(DataParserClass.data)} items URL")
+# start = time.perf_counter()
+# with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Fetching_URL') as executor:
+#     futures = [executor.submit(ItemLinkCollector, datum) for datum in data]
+#     for future in concurrent.futures.as_completed(futures):
+#         if future.result():
+#             logging.info(f"{threading.current_thread().name}) -{future.result()}")
+# final = time.perf_counter()
+# logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(DataParserClass.data)} items URL")
 
-start = time.perf_counter()
-with concurrent.futures.ThreadPoolExecutor(thread_name_prefix='Fetching_Item_Data') as executor:
-    futures = [executor.submit(DataCollectorFunction, data) for data in DataParserClass.data]
-    for future in concurrent.futures.as_completed(futures):
-        if future.result():
-            logging.info(f"{threading.current_thread().name}) -{future.result()}")
-final = time.perf_counter()
-logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  scrape  {len(DataParserClass.data)} items data")
-start = time.perf_counter()
+# start = time.perf_counter()
+# with concurrent.futures.ThreadPoolExecutor(thread_name_prefix='Fetching_Item_Data') as executor:
+#     futures = [executor.submit(DataCollectorFunction, data) for data in DataParserClass.data]
+#     for future in concurrent.futures.as_completed(futures):
+#         if future.result():
+#             logging.info(f"{threading.current_thread().name}) -{future.result()}")
+# final = time.perf_counter()
+# logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  scrape  {len(DataParserClass.data)} items data")
+# start = time.perf_counter()
 site_name = os.path.basename(__file__).split(".")[0]
 cwd = os.getcwd()
 img_dir_list = []
