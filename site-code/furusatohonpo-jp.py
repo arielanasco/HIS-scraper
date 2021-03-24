f790d16bd049323606ce6418b132a3afd2cec0f6
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-24 15:10:09 +0800

    Update furusatohonpo-jp.py

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -44,12 +44,12 @@ class ScraperCategory(WebDriver):
             self.sortNavPCCategory__itemLv2  = parent.find_elements_by_class_name("p-sortNavPCCategory__itemLv2")
             print(self.parent_category)
             print(len(self.sortNavPCCategory__itemLv2))
-            a = self.driver.find_elements_by_css_selector("frame")
-            print(len(a))
-            # for category in self.sortNavPCCategory__itemLv2:
-            #     print(f"type{category.get_attribute('innerHTML')}")
-            #     category.find_element_by_tag_name("input").click()
-            #     site.driver.current_url
+            for category in self.sortNavPCCategory__itemLv2:
+                time.sleep(5)
+                category.find_element_by_tag_name("input").click()
+                site.driver.current_url
+                time.sleep(5)
+                category.find_element_by_tag_name("input").click()
 
 
             # if len(self.sortNavPCCategory__itemLv3) != 0:
