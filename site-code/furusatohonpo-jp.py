2b036b4e0e78e8d73beb832cf9634e7a844913f9
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-24 11:33:32 +0800

    Update

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -223,52 +223,42 @@ def ItemLinkCollector(data):
 
 
 
-if __name__ == '__main__':
-    start = time.perf_counter()
-    logging.info(f"{threading.current_thread().name}) -Scraping has been started...")
-    site=ScraperCategory("https://furusatohonpo.jp/donate/s/?")
-    site.driver.get(site.url)
-    cat_container = site.driver.find_elements_by_class_name("p-sortNavPCCategory__itemLv2")
-    for cat in cat_container:
-        cat.find_element_by_tag_name("input").click()
-        catt  = cat.find_element_by_css_selector("a.js-sortAccBtn").text
-        site.categoryParser(html= site.driver.current_url, elementCat =catt)
-        cat.find_element_by_tag_name("input").click()
-    data=site.categoryList
-    save_data = SaveData()
-    for  datum in data:
-        save_data.query_db_save_catgy(datum)
-    site.driver.quit()
-    final = time.perf_counter()
-    logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} for fetching {len(data)} categories")
-    # data=[{"URL":"https://furusatohonpo.jp/donate/s/?categories=18","category":"test"}]
-    # {"URL":"https://furusatohonpo.jp/donate/s/?categories=1601","category":"test2"}]
-    # start = time.perf_counter()
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Fetching_URL') as executor:
-    #     futures = [executor.submit(ItemLinkCollector, datum) for datum in data]
-    #     for future in concurrent.futures.as_completed(futures):
-    #         if future.result():
-    #             logging.info(f"{threading.current_thread().name}) -{future.result()}")
-    # final = time.perf_counter()
-    # logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(DataParserClass.data)} items URL")
+start = time.perf_counter()
+logging.info(f"{threading.current_thread().name}) -Scraping has been started...")
+site=ScraperCategory("https://furusatohonpo.jp/donate/s/?")
+site.categoryParser(0)
+data=site.categoryList
+site.driver.quit()
+final = time.perf_counter()
+logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} for fetching {len(data)} categories")
+# data=[{"URL":"https://furusatohonpo.jp/donate/s/?categories=18","category":"test"}]
+# {"URL":"https://furusatohonpo.jp/donate/s/?categories=1601","category":"test2"}]
+# start = time.perf_counter()
+# with concurrent.futures.ThreadPoolExecutor(max_workers=8 , thread_name_prefix='Fetching_URL') as executor:
+#     futures = [executor.submit(ItemLinkCollector, datum) for datum in data]
+#     for future in concurrent.futures.as_completed(futures):
+#         if future.result():
+#             logging.info(f"{threading.current_thread().name}) -{future.result()}")
+# final = time.perf_counter()
+# logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  fetch  {len(DataParserClass.data)} items URL")
 
-    # start = time.perf_counter()
-    # with concurrent.futures.ThreadPoolExecutor(thread_name_prefix='Fetching_Item_Data') as executor:
-    #     futures = [executor.submit(DataCollectorFunction, data) for data in DataParserClass.data]
-    #     for future in concurrent.futures.as_completed(futures):
-    #         if future.result():
-    #             logging.info(f"{threading.current_thread().name}) -{future.result()}")
-    # final = time.perf_counter()
-    # logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  scrape  {len(DataParserClass.data)} items data")
+# start = time.perf_counter()
+# with concurrent.futures.ThreadPoolExecutor(thread_name_prefix='Fetching_Item_Data') as executor:
+#     futures = [executor.submit(DataCollectorFunction, data) for data in DataParserClass.data]
+#     for future in concurrent.futures.as_completed(futures):
+#         if future.result():
+#             logging.info(f"{threading.current_thread().name}) -{future.result()}")
+# final = time.perf_counter()
+# logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  scrape  {len(DataParserClass.data)} items data")
 
-    # start = time.perf_counter()
-    # site_name = os.path.basename(__file__).split(".")[0]
-    # cwd = os.getcwd()
-    # save_data = SaveData()
-    # for data_dict in DataParserClass.data:
-    #     for image_link in data_dict["images"]:
-    #         save_data.save_img(cwd,site_name,data_dict["category"],data_dict["title"],image_link)
-    #     # save_data.query_db(data_dict)
-    # final = time.perf_counter()
-    # logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  scrape  {len(DataParserClass.data)} items images")
+# start = time.perf_counter()
+# site_name = os.path.basename(__file__).split(".")[0]
+# cwd = os.getcwd()
+# save_data = SaveData()
+# for data_dict in DataParserClass.data:
+#     for image_link in data_dict["images"]:
+#         save_data.save_img(cwd,site_name,data_dict["category"],data_dict["title"],image_link)
+#     # save_data.query_db(data_dict)
+# final = time.perf_counter()
+# logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  scrape  {len(DataParserClass.data)} items images")
 
