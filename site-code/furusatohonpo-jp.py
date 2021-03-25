87a354178681e20b571d7cef576e383b6dc851aa
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-25 15:58:32 +0800

    Update furusatohonpo-jp.py

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -288,22 +288,22 @@ for  datum in data:
     mydb.commit()
 
 for  datum in DataParserClass.data:
-    # try:
-    mycursor.execute("INSERT INTO t_agt_mchan (agt_mchan_url,agt_city_nm,agt_mchan_cd,mchan_nm,mchan_desc,appli_dline,price,capacity,mchan_co,agt_cd) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-    (datum["URL"],datum["local_name"],datum["management_number"],datum["title"],datum["description"],datum["app_deadline"],datum["price"],datum["capacity"],datum["comp_name"][:30],agt_cd))
-    mydb.commit()
-    if type(datum["category"]) == list:
-        for cat in datum["category"][:8]:
-            mycursor.execute("UPDATE  t_agt_mchan SET agt_catgy_nm%s = %s  WHERE agt_mchan_url = %s",(datum['category'].index(cat)+1,cat,datum["URL"]))
-            mydb.commit()
-    else:
-        mycursor.execute("UPDATE  t_agt_mchan SET agt_catgy_nm1 = %s WHERE agt_mchan_url = %s",(datum["category"],datum["URL"]))
-        mydb.commit()       
-    for  img in datum["images"][:5]:
-        mycursor.execute("UPDATE  t_agt_mchan SET mchan_img_url%s = %s  WHERE agt_mchan_url = %s",(datum["images"].index(img)+1,img,datum["URL"]))
+    try:
+        mycursor.execute("INSERT INTO t_agt_mchan (agt_mchan_url,agt_city_nm,agt_mchan_cd,mchan_nm,mchan_desc,appli_dline,price,capacity,mchan_co,agt_cd) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (datum["URL"],datum["local_name"],datum["management_number"],datum["title"],datum["description"],datum["app_deadline"],datum["price"],datum["capacity"],datum["comp_name"][:30],agt_cd))
         mydb.commit()
-    # except:
-    #     logging.info(f"{threading.current_thread().name}) -Data failed to be saved...{datum['URL']}")
+        if type(datum["category"]) == list:
+            for cat in datum["category"][:8]:
+                mycursor.execute("UPDATE  t_agt_mchan SET agt_catgy_nm%s = %s  WHERE agt_mchan_url = %s",(datum['category'].index(cat)+1,cat,datum["URL"]))
+                mydb.commit()
+        else:
+            mycursor.execute("UPDATE  t_agt_mchan SET agt_catgy_nm1 = %s WHERE agt_mchan_url = %s",(datum["category"],datum["URL"]))
+            mydb.commit()       
+        for  img in datum["images"][:5]:
+            mycursor.execute("UPDATE  t_agt_mchan SET mchan_img_url%s = %s  WHERE agt_mchan_url = %s",(datum["images"].index(img)+1,img,datum["URL"]))
+            mydb.commit()
+    except:
+        logging.info(f"{threading.current_thread().name}) -Data failed to be saved...{datum['URL']}")
 final = time.perf_counter()
 logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} seconds to  save  {len(DataParserClass.data)} items data")
 
