57877c0022439b04b0a1655cb5c5d5bf1edc3099
Ariel Anasco <ariel@linkmind.co.jp>
2021-03-25 15:28:14 +0800

    Update furusatohonpo-jp.py

--- site-code/furusatohonpo-jp.py
+++ site-code/furusatohonpo-jp.py
@@ -256,7 +256,7 @@ site.driver.quit()
 final = time.perf_counter()
 logging.info(f"{threading.current_thread().name}) -Took {round((final-start),2)} for fetching {len(data)} categories")
 
-data=[data[0]]
+data=[data[20]]
 start = time.perf_counter()
 with concurrent.futures.ThreadPoolExecutor(max_workers=5 , thread_name_prefix='Fetching_URL') as executor:
     futures = [executor.submit(ItemLinkCollector, datum) for datum in data]
